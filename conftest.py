8307d0b3d514a02fb2ee76fc2ea00ab5e4c43250
Paul Natsuo Kishimoto <account+github@paul.kishimoto.name>
2019-06-25 10:39:55 +0200

    Add reporting module (#206)
    * add stub class for dask-based reporting
    
    * draft reporting documentation
    
    * disable py27 builds on Travis and Appveyor
    
    * Add initial tests
    
    * Update to sync with ixmp changes
    
    * Report to pyam/IAMC format
    
    * Write pyam objects to file
    
    * Use ixmp.Reporter.full_key
    
    * Install pyam & ixmp reporting branch for CI
    
    * Use setuptools.find_packages()
    
    * Appease Stickler
    
    * Install ixmp reporting branch for Appveyor CI
    
    * Give verbose output for one test failure
    
    * Include region results in test_report_as_pyam (h/t @danielhuppmann)
    
    * Overload message_ix.Reporter to add common products
    
    * Use ixmp.Reporter.add_product()
    
    * Silence linter
    
    * Add test_reporter_from_{dantzig,westeros} based on #196
    
    * Pass rename_dims through configure()
    
    * Expand documentation
    
    * Add pytest to doc/requirements.txt
    
    * Appease Stickler/flake8
    
    * Install ixmp reporting2 branch on RTD
    
    * Use iiasa:feature/reporting2 on Travis, Appveyor, RTD
    
    * Use ixmp.testing.assert_qty_equal in test_reporter (#207)
    
    * message tests now pass with updated ixmp.testing
    
    * stickler
    
    * add another westeros generation portion which adds emissions
    
    * stickler
    
    * collapse now takes a df and is not applied row-wise
    
    * changes needed with updated ixmp branch
    
    * stickler
    
    * Notice that reporting only supports Python 3.
    
    * Point to ixmp 'master' rather than 'reporting2'
    
    * Initial draft of first standard message reporting (#211)
    
    - initial draft of first standard message reporting
    - Lint
    - Collapse make_westeros_full as an option to make_westeros
    - Appease Stickler
    - Revert and silence Stickler
    - Tidy names, docs; lint
    - Avoid repeatedly adding pyam keys
    - Combine collapse methods
    - Add and use a 'concat' operation for pyam.IamDataFrame
    
    * Yet more attempts to appease Stickler
    
    * Complete documentation

--- conftest.py
+++
@@ -1,4 +0,0 @@
-# Include ixmp test fixtures, e.g. pre-populated Scenario objects
-# TODO remove this dependency
-# NB genno must follow ixmp, since pytest prefers fixtures etc. from later in the list
-pytest_plugins = ("ixmp.testing", "genno.testing")
