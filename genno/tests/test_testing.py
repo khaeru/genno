2733b04c07c3f30b9d68bd802987b2cd59bf530a
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-03-21 19:29:27 +0100

    Add assert_qty_equal(…, ignore_extra_coords=True) option

--- genno/tests/test_testing.py
+++ genno/tests/test_testing.py
@@ -2,7 +2,7 @@ import logging
 
 import pytest
 
-from genno.testing import assert_logs
+from genno.testing import assert_logs, assert_qty_allclose, assert_qty_equal
 
 log = logging.getLogger(__name__)
 
@@ -15,3 +15,12 @@ def test_assert_logs(caplog):
         log.debug("bar")
         log.info("baz")
         log.warning("spam and eggs")
+
+
+def test_assert_check_type():
+    """Mismatched types in :func:`assert_qty_equal` and :func:`assert_qty_allclose`."""
+    with pytest.raises(AssertionError):
+        assert_qty_equal(int(1), 2.2)
+
+    with pytest.raises(AssertionError):
+        assert_qty_allclose(int(1), 2.2)
