5d1f8ce352b5e6d32aeb3da84eb3c29e2878997d
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-04-27 15:33:24 +0200

    Remove outdated warnings filter

--- genno/__init__.py
+++ genno/__init__.py
@@ -1,12 +1,3 @@
-import warnings
-
-# genno.core.sparsedataarray -> sparse 0.11.2 -> numba 0.52.0 -> numpy 1.20
-warnings.filterwarnings(
-    action="ignore",
-    message="`np.long` is a deprecated alias for `np.compat.long`",
-    module="numba.core",
-)
-
 from dask.core import quote  # noqa: E402
 
 from .config import configure  # noqa: E402
