1a745760b9da1c0df40344141ef32fa488798b8b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-08-23 10:15:22 +0200

    Simplify Computation class
    - Don't inherit ABC.
    - Invoke type() to construct subclass with func.__name__.
    - Make intelligible to functools.partial() from stdlib.
    - Add repr().
    - Don't pass instance to callable.

--- genno/core/computation.py
+++ genno/core/computation.py
@@ -1,37 +1,40 @@
-from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, Callable
 
 if TYPE_CHECKING:
     from .computer import Computer
 
 
-class Computation(ABC):
-    """Abstract base class for a computation with convenience methods."""
+class Computation:
+    """Base class for a callable with convenience methods."""
 
-    # Method for adding tasks to a Computer
-    add_task: Callable
+    # Use these specific attribute names to be intelligible to functools.partial()
+    __slots__ = "func", "args", "keywords", "_add_tasks"
 
-    @abstractmethod
+    def __init__(self, func):
+        self.func = func
+        self.args = ()
+        self.kwargs = {}
+
     def __call__(self, *args, **kwargs):
-        # Actual logic of the computation
-        raise NotImplementedError  # pragma: no cover
+        # Don't pass `self` to the callable
+        return self.func(*args, **kwargs)
 
-    @classmethod
-    def helper(cls, func: Callable) -> None:
-        """Register `func` as the convenience method for creating tasks."""
-        cls.add_task = func
+    def __repr__(self):
+        return f"<{self.__class__.__name__}>"
 
-    def add_task_method(self, c: "Computer", *args, **kwargs):
-        """Invoke :attr:`add_task`; for use by Computer."""
-        return self.add_task(c, *args, **kwargs)
+    def helper(self, func: Callable) -> None:
+        """Register `func` as the convenience method for adding task(s)."""
+        self._add_tasks = func
+
+    def add_tasks(self, c: "Computer", *args, **kwargs):
+        """Invoke :attr:`_add_task` to add (a) task(s) to `c`."""
+        if self._add_tasks is None:
+            raise NotImplementedError
+
+        return self._add_tasks(self, c, *args, **kwargs)
 
 
-def computation(func):
+def computation(func: Callable):
     """Create a :class:`Computation` object that wraps `func`."""
-
-    # Create a class
-    class Foo(Computation):
-        __call__ = func
-
-    # Return an instance of the class
-    return Foo()
+    # Create a class and return an instance of it
+    return type(func.__name__, (Computation,), {})(func)
