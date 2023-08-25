0312c1396885cbd59f88332474a82908d35f1c34
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-08-25 15:52:12 +0200

    Ensure types in .compat.pyam config handling

--- genno/compat/pyam/__init__.py
+++ genno/compat/pyam/__init__.py
@@ -7,9 +7,11 @@ else:
 
 import logging
 from functools import partial
+from typing import List
 
 from genno import Computer, Key
 from genno.config import handles
+from genno.core.key import single_key
 
 log = logging.getLogger(__name__)
 
@@ -32,19 +34,21 @@ def iamc(c: Computer, info):
     name = info.pop("variable")
 
     # Chain of keys produced: first entry is the key for the base quantity
-    keys = [Key(info.pop("base"))]
+    keys: List[Key] = [Key(info.pop("base"))]
 
     # Second entry is a simple rename
-    keys.append(c.add_single(Key(name, keys[0].dims, keys[0].tag), keys[0]))
+    keys.append(single_key(c.add_single(Key(name, keys[0].dims, keys[0].tag), keys[0])))
 
     # Optionally select a subset of data from the base quantity
     sel = info.get("select")
     if sel:
         keys.append(
-            c.add_single(
-                keys[-1].add_tag("sel"),
-                (c.get_comp("select"), keys[-1], sel),
-                strict=True,
+            single_key(
+                c.add_single(
+                    keys[-1].add_tag("sel"),
+                    (c.get_comp("select"), keys[-1], sel),
+                    strict=True,
+                )
             )
         )
 
@@ -56,13 +60,16 @@ def iamc(c: Computer, info):
     # Use the Computer method to add the coversion step
     # NB convert_pyam() returns a single key when applied to a single key
     keys.append(
-        c.convert_pyam(
-            keys[-1],
-            rename=info.pop("rename", {}),
-            collapse=partial(collapse_func, **collapse_info),
-            replace=info.pop("replace", {}),
-            drop=set(info.pop("drop", [])) & set(keys[-1].dims),
-            unit=info.pop("unit", None),
+        single_key(
+            c.add(
+                keys[-1],
+                "as_pyam",
+                rename=info.pop("rename", {}),
+                collapse=partial(collapse_func, **collapse_info),
+                replace=info.pop("replace", {}),
+                drop=set(info.pop("drop", [])) & set(keys[-1].dims),
+                unit=info.pop("unit", None),
+            )
         )
     )
 
